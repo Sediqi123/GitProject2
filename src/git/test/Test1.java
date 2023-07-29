f32add9c7fb85c2f99568d9f162336241ed364ef
khsed <khsed@10.0.0.89>
2023-07-29 11:31:36 -0400

    This is my first test class

---
+++ src/git/test/Test1.java
@@ -0,0 +1,19 @@
+package git.test;
+
+public class Test1 {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("This is my first GitHub test");
+		
+		
+		
+		
+	}
+	
+	public static void getInfo() {
+		
+		System.out.println("Print information");
+	}
+
+}
